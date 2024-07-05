203b2a913640dff7df2c77f9c7914dfb4b65a34c
ashvn24 <ashwinashi6@gmail.com>
2024-07-05 07:28:22 +0530

    notification for user-creation

--- consumer.py
+++ consumer.py
@@ -1,5 +1,12 @@
 import pika
-import os
+import os, json, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notification_service.settings')
+
+# Initialize Django
+django.setup()
+
+from service.models import Notification
 
 # Retrieve the CloudAMQP URL from environment variables
 url = os.getenv('CLOUDAMQP_URL')
@@ -10,14 +17,23 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 # Declare the queue
-channel.queue_declare(queue='user')
+channel.queue_declare(queue='notification')
 
 # Define the callback function
 def callback(ch, method, properties, body):
-    print(f"Received: {body}")
+    print(f"Received in notification-service")
+    data =json.loads(body)
+    
+    if properties.content_type == 'user-created':
+        db = Notification.objects.create(
+            title='user-created',
+            message = data['email']
+        )
+        db.save()
+        print('user-created')
 
 # Set up subscription on the queue
-channel.basic_consume(queue='user', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='notification', on_message_callback=callback, auto_ack=True)
 
 print("Started Consuming")
 
